7f37e4cf09617b44089508c00c899a92d8d3588a
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-01-17 21:21:09 -0800

    RandomGoalSwimmerEnv import

--- sac/envs/__init__.py
+++ sac/envs/__init__.py
@@ -1,4 +1,5 @@
 from .gym_env import GymEnv
 from .multi_direction_swimmer_env import MultiDirectionSwimmerEnv
+from .random_goal_swimmer_env import RandomGoalSwimmerEnv
 from .multi_direction_ant_env import MultiDirectionAntEnv
 from .multigoal import MultiGoalEnv
