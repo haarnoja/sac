b097c08cf673f05bde78a82debddb2edc15d9aa7
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-01-28 16:10:06 -0800

    Add new HierarchyProxyEnv

--- sac/envs/__init__.py
+++ sac/envs/__init__.py
@@ -1,5 +1,6 @@
 from .gym_env import GymEnv
 from .multi_direction_swimmer_env import MultiDirectionSwimmerEnv
 from .random_goal_swimmer_env import RandomGoalSwimmerEnv
+from .hierarchy_proxy_env import HierarchyProxyEnv
 from .multi_direction_ant_env import MultiDirectionAntEnv
 from .multigoal import MultiGoalEnv
