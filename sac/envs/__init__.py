a2fe4c40d610ad64af182955ee1febd3c5abbb67
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-04-08 20:29:04 -0700

    Fix some rebase typos

--- sac/envs/__init__.py
+++ sac/envs/__init__.py
@@ -3,13 +3,7 @@ from .multi_direction_env import (
     MultiDirectionSwimmerEnv,
     MultiDirectionAntEnv,
     MultiDirectionHumanoidEnv)
-from .random_goal_swimmer_env import RandomGoalSwimmerEnv
-from .random_goal_ant_env import RandomGoalAntEnv
-from .random_goal_humanoid_env import RandomGoalHumanoidEnv
-from .random_wall_ant_env import RandomWallAntEnv
 
 from .cross_maze_ant_env import CrossMazeAntEnv
-from .simple_maze_ant_env import SimpleMazeAntEnv
-
 from .hierarchy_proxy_env import HierarchyProxyEnv
 from .multigoal import MultiGoalEnv
